bf1a46f6959713476c7a1d292ec9e8ecb87d45fa
ainur-fa <ainur_fa@mail.ru>
2016-10-04 05:56:59 +0500

    Созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
-from application import Application
 import pytest
+
+from fixture.application import Application
+from model.contact import Contact
+
 
 @pytest.fixture
 def app(request):

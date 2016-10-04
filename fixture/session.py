cacc6a7afef6ab6cee0d8208d8280fcaff399a8d
ainur-fa <ainur_fa@mail.ru>
2016-10-04 06:54:15 +0500

    Двухуровневая архитектура тестов добавления группы и контакта.

--- fixture/session.py
+++ fixture/session.py
@@ -1,9 +1,8 @@
-
+# -*- coding: utf-8 -*-
 class SessionHelper:
 
     def __init__(self, app):
         self.app = app
-
 
     def login(self, username, password):
         wd = self.app.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,12 +12,12 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="erhherwh", lastname="werhwerh", address="erherhewh", mobile="346346343", email="ergergerger34"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="erhherwh", lastname="werhwerh", address="erherhewh", mobile="346346343", email="ergergerger34"))
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", lastname="", address="", mobile="", email=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", lastname="", address="", mobile="", email=""))
+    app.session.logout()
 

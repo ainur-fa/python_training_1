11bb17869ffb57ca3ad9a95b199baed4ee3d0c42
ainur-fa <ainur_fa@mail.ru>
2016-10-06 06:18:44 +0500

    Общая фикстура для всех тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
@@ -20,4 +10,3 @@ def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", address="", mobile="", email=""))
     app.session.logout()
-

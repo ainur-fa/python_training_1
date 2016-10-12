fb891198f1c434a96d545080f3eb81c99f4df675
ainur-fa <ainur_fa@mail.ru>
2016-10-12 22:23:49 +0500

    Добавление предусловий во все тесты.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="tester", email="test@test", mobile="911"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,11 @@
 from model.contact import Contact
 
 def test_modify_contact_email(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname = "tester",email="test@test", mobile = "911"))
     app.contact.modify_first_contact(Contact(email="666999@mail.ru"))
 
 def test_modify_contact_mobile(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname = "tester", email="test@test", mobile = "911"))
     app.contact.modify_first_contact(Contact(mobile="666000"))

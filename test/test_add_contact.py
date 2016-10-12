296960925a76345b83660003756bd11660217733
ainur-fa <ainur_fa@mail.ru>
2016-10-12 20:50:15 +0500

    Перенос login и logout в conftest

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,7 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="erhherwh", lastname="werhwerh", address="erherhewh", mobile="346346343", email="ergergerger34"))
-    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", address="", mobile="", email=""))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,11 +2,7 @@
 from model.contact import Contact
 
 def test_modify_contact_email(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(email="666999@mail.ru"))
-    app.session.logout()
 
 def test_modify_contact_mobile(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(mobile="666000"))
-    app.session.logout()

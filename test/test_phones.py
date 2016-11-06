c9da41de2d13438ecf20327b9fc775974efa90ad
ainur-fa <ainur_fa@mail.ru>
2016-11-06 22:10:57 +0500

    Changed "merge method" for emails

--- test/test_phones.py
+++ test/test_phones.py
@@ -5,8 +5,8 @@ def test_fields_on_home_page(app):
     index = randrange(len(app.contact.get_contact_list()))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_fields_like_on_home_page(contact_from_edit_page, type="phones")
-    assert contact_from_home_page.all_emails_from_home_page == merge_fields_like_on_home_page(contact_from_edit_page, type="emails")
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
@@ -22,9 +22,11 @@ def test_phones_on_contact_view_page(app):
 def clear(s):
         return re.sub("[() -]","", s)
 
-def merge_fields_like_on_home_page(contact, type):
-    if type == "phones": merged = [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]
-    elif type == "emails": merged = [contact.email, contact.email2, contact.email3]
+def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
-                                filter(lambda x: x is not None, merged))))
+                                filter(lambda x: x is not None, [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None,
+                                                      (map(lambda x: x.rstrip(), [contact.email, contact.email2, contact.email3])))))

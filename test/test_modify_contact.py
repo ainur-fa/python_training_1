9c474250137191367f12d1107a21b491832ae33d
ainur-fa <ainur_fa@mail.ru>
2016-11-20 14:32:43 +0500

    Сравнение данных из БД(контакты+группы)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 from random import randrange
 from model.contact import Contact
+import random
 
-def test_modify_contact_firstname(app):
+def test_modify_contact_firstname(app, db, check_ui):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname = "tester",email="test@test", mobilephone = "911"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact = Contact(firstname="Test666999")
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(index, contact)
-    new_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    new_contact_data = Contact(firstname="Test666999")
+    app.contact.modify_contact_by_id(contact.id, new_contact_data)
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[index] = contact
+    for x in range(len(old_contacts)):
+        if old_contacts[x].id == contact.id:
+            index = x
+    old_contacts[index].firstname = new_contact_data.firstname
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
 # def test_modify_contact_mobile(app):
 #     if app.contact.count() == 0:
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from random import randrange
 from model.group import Group
+import random
 
 def test_modify_group_name(app, db, check_ui):
     if app.group.count() == 0:
         app.group.create(Group(name = "test"))
     old_groups = db.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
+    group = random.choice(old_groups)
+    new_group_data = Group(name="New group")
+    app.group.modify_group_by_id(group.id, new_group_data)
     new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    for x in range(len(old_groups)):
+        if old_groups[x].id == group.id:
+            index = x
+    old_groups[index].name = new_group_data.name
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)

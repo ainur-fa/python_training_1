351e37619cf1d549d355c445adaa0371be779251
ainur-fa <ainur_fa@mail.ru>
2016-11-20 18:01:27 +0500

    Проверка информации о контактах на глав.странице(инфа из БД)

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,19 +1,19 @@
 import re
-from random import randrange
+from model.contact import Contact
 
-def test_fields_on_home_page(app):
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+def test_fields_on_home_page(app, db):
+    for i in range(len(db.get_contact_fields())):
+        contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)[i]
+        contacts_from_db = sorted(db.get_contact_fields(), key=Contact.id_or_max)[i]
+        assert contacts_from_home_page.firstname == contacts_from_db.firstname
+        assert contacts_from_home_page.lastname == contacts_from_db.lastname
+        assert contacts_from_home_page.address == contacts_from_db.address
+        assert contacts_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db)
+        assert contacts_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db)
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact_from_view_page = app.contact.get_contact_from_view_page(1)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(1)
     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone

6abffdae154e7bef0f3d417240e989a235d6341f
ainur-fa <ainur_fa@mail.ru>
2016-11-21 23:07:10 +0500

    test_add_contact_to_group

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 from model.group import Group
 import time
 from fixture.orm import ORMFixture
-db1 = ORMFixture(host="127.0.0.1", name="addressbook", user="root",password="")
+db_orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root",password="")
 
 import random
 
@@ -13,8 +13,8 @@ def test_add_contact_to_group(app, db):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name = "test"))
     group = random.choice(db.get_group_list())
-    contacts_not_in_group = db1.get_contacts_not_in_group(Group(id=group.id))
+    contacts_not_in_group = db_orm.get_contacts_not_in_group(Group(id=group.id))
     contact = random.choice(contacts_not_in_group)
     app.contact.add_contact_to_group(contact.id, group.name)
-    contacts_in_group = db1.get_contacts_in_group(Group(id=group.id))
+    contacts_in_group = db_orm.get_contacts_in_group(Group(id=group.id))
     assert contact in contacts_in_group

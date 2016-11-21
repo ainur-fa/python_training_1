16270465ff21c29545770a5ef59ceab4d4facacd
ainur-fa <ainur_fa@mail.ru>
2016-11-21 22:36:49 +0500

    remake "test_phones"

--- test/test_phones.py
+++ test/test_phones.py
@@ -2,14 +2,14 @@ import re
 from model.contact import Contact
 
 def test_fields_on_home_page(app, db):
-    for i in range(len(db.get_contact_fields())):
-        contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)[i]
-        contacts_from_db = sorted(db.get_contact_fields(), key=Contact.id_or_max)[i]
-        assert contacts_from_home_page.firstname == contacts_from_db.firstname
-        assert contacts_from_home_page.lastname == contacts_from_db.lastname
-        assert contacts_from_home_page.address == contacts_from_db.address
-        assert contacts_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db)
-        assert contacts_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_fields(), key=Contact.id_or_max)
+    for i in range(len(contacts_from_db)):
+        assert contacts_from_home_page[i].firstname == contacts_from_db[i].firstname
+        assert contacts_from_home_page[i].lastname == contacts_from_db[i].lastname
+        assert contacts_from_home_page[i].address == contacts_from_db[i].address
+        assert contacts_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db[i])
+        assert contacts_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db[i])
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(1)

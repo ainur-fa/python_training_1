94db4dbdaff552d6220225cb7c58ef62aa4cbebb
ainur-fa <ainur_fa@mail.ru>
2016-12-02 21:14:20 +0500

    refactor orm

--- test/test_del_contact_in_group.py
+++ test/test_del_contact_in_group.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 from model.group import Group
-from fixture.orm import ORMFixture
-db_orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root",password="")
 import random
 
-def test_del_contact_in_group(app, db):
+def test_del_contact_in_group(app, db, orm):
     list =[]
     app.contact.check_available_min_requirement(app, db)
     group_list = db.get_group_list()
     for this_group in group_list:
-        contacts_in_group = db_orm.get_contacts_in_group(Group(id=this_group.id))
+        contacts_in_group = orm.get_contacts_in_group(Group(id=this_group.id))
         [list.append(elem) for elem in contacts_in_group if elem not in list]
 
     if list ==[]:
         group = random.choice(db.get_group_list())
         contact = random.choice(db.get_contact_list())
         app.contact.add_contact_to_group(contact.id, group.name)
-        list.append(db_orm.get_contact_list()[0])
+        list.append(orm.get_contact_list()[0])
 
     contact = random.choice(list)
     print(contact)
-    group =  random.choice(db_orm.get_group_where_contact(Contact(id=contact.id)))
+    group =  random.choice(orm.get_group_where_contact(Contact(id=contact.id)))
     print(group)
     app.contact.del_contact_in_group(contact.id, group.id)
-    # assert contact not in db_orm.get_contacts_in_group(Group(id=group.id))
+    assert contact not in orm.get_contacts_in_group(Group(id=group.id))
